012889e7e8dafa43f15bcd18a0306fd60fac2858
tim90721 <tim90721@gmail.com>
2019-01-30 13:48:30 +0800

    modify run.sh when make error happend, it won't execute

--- run.sh
+++ run.sh
@@ -7,6 +7,5 @@ qmake -project -o ${buildDir}/${qt_project_filename}
 sed -i '$a QT += widgets' ${buildDir}/${qt_project_filename}
 sed -i 's/INCLUDEPATH.*$/INCLUDEPATH += \/home\/daitor\/Qt\/5.12.0\/gcc_64\/include \./g' ${buildDir}/${qt_project_filename}
 qmake ${buildDir}/${qt_project_filename} -o ${buildDir}
-make -C ${buildDir} 
-./${buildDir}/${project_name}
+make -C ${buildDir} && ./${buildDir}/${project_name}
 
