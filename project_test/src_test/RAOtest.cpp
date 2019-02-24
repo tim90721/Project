220745a40cdea8a0e152830a13b2ec4c370f6c0d
tim90721 <tim90721@gmail.com>
2019-02-24 21:40:59 +0800

    first test complete

--- project_test/src_test/RAOtest.cpp
+++ project_test/src_test/RAOtest.cpp
@@ -20,14 +20,69 @@ void initialize(){
 void test1(){
     initialize();
     cell->setPrachConfigIndex(16);
+    cell->setMsg1FDM(1);
+    cell->setSSBPerRAO(1);
     printf("msg1FDM: %d\n", cell->getMsg1FDM());
     printf("ssb-perRAO: %f\n", cell->getSSBPerRAO());
     cell->broadcastSI();
+
     int simulationTime = 60;
+    vector<vector<int>> expected;
+    int i = 0;
+    vector<int> *subframeExpect;
+    while(i < 11){
+        subframeExpect = new vector<int>;
+        expected.push_back(*subframeExpect);
+        i++;
+    }
+    subframeExpect = new vector<int>;
+    subframeExpect->push_back(1);
+    expected.push_back(*subframeExpect);
+    i++;
+    while(i < 51){
+        subframeExpect = new vector<int>;
+        expected.push_back(*subframeExpect);
+        i++;
+    }
+    subframeExpect = new vector<int>;
+    subframeExpect->push_back(1);
+    expected.push_back(*subframeExpect);
+    i++;
+    while(i < 60){
+        subframeExpect = new vector<int>;
+        expected.push_back(*subframeExpect);
+        i++;
+    }
+
     for(int i = 0;i < simulationTime;i++){
         printf("===================info===================\n");
         cell->broadcastSI();
         ue->doRA();
+        vector<int>& raos = ue->getRAOs();
+        vector<int> expect = expected[i];
+        bool same = true;
+        if(raos.size() == expect.size()){
+            printf("same size\n");
+            for(unsigned int j = 0;j < raos.size();j++){
+                if(raos[j] == expect[j])
+                    continue;
+                else{
+                    same = false;
+                    break;
+                }
+            }
+        }
+        else
+            same = false;
+        if(same){
+            printf("correct\n");
+        }
+        else{
+            printf("%d\t%d\n", raos.size(), expect.size());
+            printf("wrong\n");
+            return;
+        }
+        
         cell->updateSubframe();
         printf("===================info===================\n");
     }
