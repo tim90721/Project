b0465a88769d2cb890bef2caf9275d0aa25fa5ee
tim90721 <tim90721@gmail.com>
2019-02-24 17:08:12 +0800

    testing project

---
+++ project_test/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo $project_name
+qt_project_filename="${project_name}.pro"
+buildDir=./build_test
+qmake -project -o ${buildDir}/${qt_project_filename}
+sed -i '$a QT += widgets' ${buildDir}/${qt_project_filename}
+sed -i 's/INCLUDEPATH.*$/INCLUDEPATH += \/home\/daitor\/Qt\/5.12.0\/gcc_64\/include \./g' ${buildDir}/${qt_project_filename}
+qmake ${buildDir}/${qt_project_filename} -o ${buildDir}
+make -C ${buildDir} && ./${buildDir}/${project_name}
+
