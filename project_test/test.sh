64103ae52b0eb922b5ea15e0fe2776aa02484025
tim90721 <tim90721@gmail.com>
2019-02-24 19:43:34 +0800

    wisely select newer for to compile

--- project_test/test.sh
+++ project_test/test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-echo $project_name
+echo "project name: $project_name test"
 qt_project_filename="${project_name}.pro"
 buildDir=./build_test
+newestFileinTestSrc=$(ls -t src/ | head -1)
+echo "newest file $newestFileinTestSrc"
+newerFile=$(find ../main/src/ -newer src/$newestFileinTestSrc | grep -E '.*cpp$|.*h$')
+if [ "${newerFile}" != "" ]; then
+    echo "newer file $newerFile"
+    cp $newerFile ./src/
+else
+    echo "no new file"
+fi
+
+#cp ../main/src/*.h ./src/
+test -e ./src/mainGUI.h && rm ./src/mainGUI.h
+test -e ./src/mainGUI.cpp && rm ./src/mainGUI.cpp
+test -e ./src/main.cpp && rm ./src/main.cpp
+#rm ./src/mainGUI.h
+#rm ./src/mainGUI.cpp
+#rm ./src/main.cpp
 qmake -project -o ${buildDir}/${qt_project_filename}
 sed -i '$a QT += widgets' ${buildDir}/${qt_project_filename}
 sed -i 's/INCLUDEPATH.*$/INCLUDEPATH += \/home\/daitor\/Qt\/5.12.0\/gcc_64\/include \./g' ${buildDir}/${qt_project_filename}
