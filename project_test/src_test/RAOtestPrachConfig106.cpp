3df629c16e3471ff8c875fee0da0d0e3752254d0
tim90721 <tim90721@gmail.com>
2019-02-27 13:48:59 +0800

    complete prach index 106 test case

--- project_test/src_test/RAOtestPrachConfig106.cpp
+++ project_test/src_test/RAOtestPrachConfig106.cpp
@@ -6,6 +6,344 @@
 extern Cell *cell;
 extern UE *ue;
 
+void testPrachIndex106_23(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 8;
+    double ssbPerRAO = 4;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 96;k++){
+                subframeExpect->push_back(k);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_22(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 2;
+    double ssbPerRAO = 4;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 24;k++){
+                subframeExpect->push_back(k);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_21(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 1;
+    double ssbPerRAO = 4;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 12;k++){
+                subframeExpect->push_back(k);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_20(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 8;
+    double ssbPerRAO = 2;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 48;k++){
+                subframeExpect->push_back(k * 2);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_19(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 4;
+    double ssbPerRAO = 2;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 24;k++){
+                subframeExpect->push_back(k * 2);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_18(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 2;
+    double ssbPerRAO = 2;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 12;k++){
+                subframeExpect->push_back(k * 2);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_17(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 1;
+    double ssbPerRAO = 2;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 0;k < 6;k++){
+                subframeExpect->push_back(k * 2);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_16(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 8;
+    double ssbPerRAO = 1.0 / 8.0;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 10;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 8;k < 16;k++){
+                subframeExpect->push_back(k);
+            }
+            for(int k = 40;k < 48;k++){
+                subframeExpect->push_back(k);
+            }
+            for(int k = 72;k < 80;k++){
+                subframeExpect->push_back(k);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
+void testPrachIndex106_15(){
+    int prachConfigIndex = 106;
+    int msg1FDM = 4;
+    double ssbPerRAO = 1.0 / 8.0;
+    initialize(prachConfigIndex, msg1FDM, ssbPerRAO);
+
+    int simulationTime = 100;
+    int associationFrame = 1;
+    vector<vector<int>> expected;
+
+    vector<int> *subframeExpect;
+    int round = getRound(simulationTime, associationFrame);
+    for(int i = 0;i < round;i++){
+        int j = i * associationFrame * 10;
+        
+        for(int l = 0;l < 5;l++){
+            subframeExpect = new vector<int>;
+            for(int k = 8;k < 16;k++){
+                subframeExpect->push_back(k);
+            }
+            for(int k = 40;k < 48;k++){
+                subframeExpect->push_back(k);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+
+            subframeExpect = new vector<int>;
+            for(int k = 40;k < 48;k++){
+                subframeExpect->push_back(k);
+            }
+            expected.push_back(*subframeExpect);
+            j++;
+        }
+        while(j % (associationFrame * 10) != 0){
+            subframeExpect = new vector<int>;
+            expected.push_back(*subframeExpect);
+            j++;
+            if(j >= simulationTime)
+                break;
+        }
+    }
+    validation(expected, simulationTime, prachConfigIndex, msg1FDM, ssbPerRAO);
+    destory();
+}
+
 void testPrachIndex106_14(){
     int prachConfigIndex = 106;
     int msg1FDM = 2;
@@ -630,6 +968,15 @@ void testPrachIndex106(){
     testPrachIndex106_12();
     testPrachIndex106_13();
     testPrachIndex106_14();
+    testPrachIndex106_15();
+    testPrachIndex106_16();
+    testPrachIndex106_17();
+    testPrachIndex106_18();
+    testPrachIndex106_19();
+    testPrachIndex106_20();
+    testPrachIndex106_21();
+    testPrachIndex106_22();
+    testPrachIndex106_23();
 }
 #endif
 
