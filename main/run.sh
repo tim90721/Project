80257882c93b752742fbfafed77d7844eefdf72a
tim90721 <tim90721@gmail.com>
2019-04-09 16:30:09 +0800

    modify to include log function file

--- main/run.sh
+++ main/run.sh
@@ -7,6 +7,11 @@ qmake -project -o ${buildDir}/${qt_project_filename}
 sed -i '$a QT += widgets' ${buildDir}/${qt_project_filename}
 sed -i 's/INCLUDEPATH.*$/INCLUDEPATH += \/home\/daitor\/Qt\/5.12.0\/gcc_64\/include \.\.\/src \.\.\/include /g' ${buildDir}/${qt_project_filename}
 #sed -i 's/INCLUDEPATH.*$/INCLUDEPATH += \/home\/daitor\/Qt\/5.12.0\/gcc_64\/include \.\/src /g' ${buildDir}/${qt_project_filename}
+if [ ! -d "./log/" ]; then
+    echo "log directory does not exist"
+    mkdir log
+fi
+
 qmake ${buildDir}/${qt_project_filename} -o ${buildDir}
 make -C ${buildDir} && ./${buildDir}/${project_name}
 
