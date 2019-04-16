2831280bb69c229739a0f85fc0976d96b6700531
tim90721 <tim90721@gmail.com>
2019-04-16 14:57:05 +0800

    plot cdf

--- main/scripts/evaluateResult.py
+++ main/scripts/evaluateResult.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from plot_result import collectDataUE
 
+figureCount = 0
+
 def getSubframePeriod(prachIndex):
     if prachIndex == 27:
         return 1
@@ -17,8 +19,10 @@ def getSubframePeriod(prachIndex):
     elif prachIndex == 16:
         return 10
 
-def plotAverageResult(average):
-    fig = plt.figure(1)
+def plotAverageResult(average, filename=""):
+    global figureCount
+    fig = plt.figure(figureCount)
+    figureCount = figureCount + 1
     fig.subplots_adjust(top = 0.85)
     ax = plt.subplot(1, 1, 1)
     plt.plot(average.keys(), average.values(), 'b-o')
@@ -27,32 +31,76 @@ def plotAverageResult(average):
     plt.suptitle("Average UE Latency vs RA Subframe Period", fontsize=14, fontweight="bold")
     plt.axis([0, max(average.keys()) + 2, 0, max(average.values()) + 10])
     plt.grid(True)
-    plt.show()
+    if filename:
+        plt.savefig(filename)
+    #plt.show()
+
+def plotLantencyCDF(latency, prachIndex, simulationTime, arrivalRate, saveFolderName=""):
+    global figureCount
+    subTitle = "Prach Configuration Index: {0}\n".format(prachIndex) \
+            + "Simulation Time: {0} ".format(str(int(simulationTime) / 1000)) \
+            + "UE Arrival Rate: {0}\n".format(arrivalRate)
+    filenameFig = "CDF_prach-{0}_simu-{1}_arrival-{2}".format(prachIndex,
+            simulationTime,
+            arrivalRate)
+
+    X = np.linspace(min(latency), max(latency), max(latency) - min(latency))
+    hist, bin_edges = np.histogram(latency, bins=max(latency) - min(latency), density=True)
+    hist = np.cumsum(hist)
+
+    fig = plt.figure(figureCount)
+    figureCount = figureCount + 1
+    fig.subplots_adjust(top=0.85)
+    ax = plt.subplot(1, 1, 1)
+    plt.plot(X, hist, 'b-')
+    plt.xlabel("Latency (ms)")
+    plt.ylabel("CDF")
+    plt.suptitle("UE Latency CDF", fontsize=14, fontweight="bold")
+    ax.set_title(subTitle, y=0.93)
+    plt.axis([min(latency), max(latency), 0, 1.1])
+    plt.grid(True)
+    if saveFolderName:
+        plt.savefig(saveFolderName + filenameFig)
+    del X
+    del hist
+    del bin_edges
 
 ueFile = 'UE.csv'
 cellFile = 'Cell.csv'
 resultSourceFolder = "./candidateResult/"
+savefigureName = "result.png"
 
 folderName = [name for name in os.listdir(resultSourceFolder)]
+folderName.remove(savefigureName)
 
+############ get parameters ############
 prachIndex = [name.split("prach-")[1] for name in folderName]
 prachIndex = [name.split("_")[0] for name in prachIndex]
+simulationTime = [name.split("simu-")[1] for name in folderName]
+simulationTime = [name.split("_")[0] for name in simulationTime]
+arrivalRate = [name.split("arrival-")[1] for name in folderName]
+arrivalRate = [name.split("_")[0] for name in arrivalRate]
+############ get parameters ############
 
 avgs = {}
+latencies = []
 
 while(len(prachIndex) > 0):
     maxIndex = prachIndex.index(max(prachIndex))
     filename = resultSourceFolder + folderName[maxIndex] + "/" + ueFile
-    latencies = collectDataUE(filename)
-    avgs[getSubframePeriod(int(prachIndex[maxIndex]))] = np.mean(list(latencies.values()))
+    latency = collectDataUE(filename)
+    avgs[getSubframePeriod(int(prachIndex[maxIndex]))] = np.mean(list(latency.values()))
+    plotLantencyCDF(list(latency.values()), prachIndex[maxIndex], simulationTime[maxIndex], arrivalRate[maxIndex], resultSourceFolder)
     #print(prachIndex[maxIndex])
     #print(folderName[maxIndex])
     #print(filename)
-    #print(len(latencies))
+    #print(len(latency))
     del prachIndex[maxIndex]
+    del simulationTime[maxIndex]
+    del arrivalRate[maxIndex]
     del folderName[maxIndex]
-    del latencies
 
 print(avgs)
-plotAverageResult(avgs)
+plotAverageResult(avgs, resultSourceFolder + savefigureName)
+plt.show()
 del avgs
